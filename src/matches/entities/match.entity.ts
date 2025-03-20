e5ec9b2ec986295382ced8b5a4676126037afe51
Pablo Ferreira <pablomartinferre@gmail.com>
2025-03-20 10:35:35 -0300

    Match entity

--- src/matches/entities/match.entity.ts
+++ src/matches/entities/match.entity.ts
@@ -1,1 +1,25 @@
-export class Match {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Match {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    player1id: number;
+
+    @Column()
+    player2id: number;
+
+    @Column()
+    startTime: Date;
+
+    @Column()
+    endTime: Date;
+
+    @Column()
+    winnerId: number;
+
+    @Column()
+    tableNumber: number;
+}
