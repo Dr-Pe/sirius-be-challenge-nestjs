4cc763ab5a8d31f87901169c044d3c840417bd55
Pablo Ferreira <pablomartinferre@gmail.com>
2025-03-20 10:33:09 -0300

    Matches module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Match } from './matches/entities/match.entity';
+import { MatchesModule } from './matches/matches.module';
 import { Player } from './players/entities/player.entity';
 import { PlayersModule } from './players/players.module';
 
@@ -11,11 +13,13 @@ import { PlayersModule } from './players/players.module';
       type: 'sqlite',
       database: 'test',
       entities: [
+        Match,
         Player,
       ],
       synchronize: true, // WARNING: DO NOT USE IN PRODUCTION
     }),
     PlayersModule,
+    MatchesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
